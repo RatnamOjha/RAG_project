646e8ffda218aa99d41a364358814e8b357d7fd9
Ratnam Ojha <ratnamojha71@gmail.com>
2025-10-09 00:52:05 +0530

    chunking -> saving embeddings

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from src.data_loader import load_all_documents
+from src.embedding import EmbeddingPipeline
 
 if __name__ == "__main__":
+    
     docs = load_all_documents("data")
-    print("Docs:", docs)
-    print(f"Loaded {len(docs)} documents.")
-    print("Example document:", docs[0] if docs else None)
+    emb_pipe = EmbeddingPipeline()
+    chunks = emb_pipe.chunk_documents(docs)
+    embeddings = emb_pipe.embed_chunks(chunks)
+    print("[INFO] Example embedding:", embeddings[0] if len(embeddings) > 0 else None)
