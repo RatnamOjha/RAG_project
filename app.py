80b45e1c6a93e29b3a986ae859b84aca09990e2c
Ratnam Ojha <ratnamojha71@gmail.com>
2025-10-09 01:48:30 +0530

    creating vectorstore

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from src.data_loader import load_all_documents
 from src.embedding import EmbeddingPipeline
+from src.vectorstore import FaissVectorStore
+from src.search import RAGSearch
 
 if __name__ == "__main__":
-    
-    docs = load_all_documents("data")
-    emb_pipe = EmbeddingPipeline()
-    chunks = emb_pipe.chunk_documents(docs)
-    embeddings = emb_pipe.embed_chunks(chunks)
-    print("[INFO] Example embedding:", embeddings[0] if len(embeddings) > 0 else None)
+    rag_search = RAGSearch()
+    query = "What is attention mechanism?"
+    summary = rag_search.search_and_summarize(query, top_k=3)
+    print("Summary:", summary)
